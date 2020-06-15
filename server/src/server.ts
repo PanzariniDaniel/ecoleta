b6bebb20bb35979d13825fa8179d62d2ce90b209
Daniel Panzarini <panzarinidaniel@outlook.com>
2020-06-15 18:50:52 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { errors } from 'celebrate'
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+// rota estatica para imagens
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
+
+app.listen(3333);
